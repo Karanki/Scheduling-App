b6faea4bb4890d5b5017969cca097e2deb46b8bd
zilin <zi@zilins-air.wifi.bcit.ca>
2015-11-01 18:08:32 -0800

    I made some changes on the index file; add a javascript file and put it into a folder named js.

---
+++ OD_HTML/js/index.js
@@ -0,0 +1,26 @@
+
+
+
+function hideSignup(){
+
+    document.getElementById("loginOD").style.visibility = "hidden";
+    document.getElementById("signupOD").style.visibility = "hidden";
+    document.getElementById("dLogin").style.visibility = "visible";
+    document.getElementById("dLogin").style.display = "";
+    document.getElementById("dMain").style.display = "none";
+
+};
+
+
+
+function hideLogin(){
+
+    document.getElementById("loginOD").style.visibility = "hidden";
+    document.getElementById("signupOD").style.visibility = "hidden";
+    document.getElementById("dSignup").style.visibility = "visible";
+    document.getElementById("dSignup").style.display = "";
+    document.getElementById("dMain").style.display = "none";
+    
+   
+
+};
