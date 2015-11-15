d05bc7eef4d78690aae821cc6cf2c8ddad6dc9d0
Andres Karolys <andreskarolys@gmail.com>
2015-11-14 17:40:24 -0800

    Added back button to login pages

--- OD_HTML/js/index.js
+++ OD_HTML/js/index.js
@@ -8,7 +8,7 @@ function hideSignup(){
     document.getElementById("dLogin").style.visibility = "visible";
     document.getElementById("dLogin").style.display = "";
     document.getElementById("dMain").style.display = "none";
-
+    document.getElementById("bBtn").style.visibility = "visible";
 };
 
 
@@ -20,7 +20,18 @@ function hideLogin(){
     document.getElementById("dSignup").style.visibility = "visible";
     document.getElementById("dSignup").style.display = "";
     document.getElementById("dMain").style.display = "none";
-    
-   
-
+    document.getElementById("bBtn").style.visibility = "visible";
 };
+
+
+$("#bBtn").click(
+function showMain(){
+    document.getElementById("loginOD").style.visibility = "visible";
+    document.getElementById("signupOD").style.visibility = "visible";
+    document.getElementById("dLogin").style.visibility = "hidden";
+    document.getElementById("dLogin").style.display = "none";
+    document.getElementById("dSignup").style.visibility = "hidden";
+    document.getElementById("dSignup").style.display = "none";
+    document.getElementById("dMain").style.display = "";
+    document.getElementById("bBtn").style.visibility = "hidden";
+ });
